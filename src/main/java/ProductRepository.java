8d6a4de0024a1733228dc4552c7579bc9d0799d8
Levon Poghosyan <levon9393@bk.ru>
2022-08-13 18:28:19 +0300

    Services added

---
+++ src/main/java/ProductRepository.java
@@ -0,0 +1,40 @@
+public class ProductRepository {
+
+    private Product[] items = new Product[0];
+
+    public void save(Product item) {
+        Product[] tmp = new Product [items.length +1];
+        for (int i = 0; i < items.length; i++) {
+            tmp[i] = items[i];
+        }
+        tmp[tmp.length -1] = item;
+        items = tmp;
+    }
+
+    public Product[] findAll() {
+        return items;
+    }
+
+    public Product findById(int id) {
+        for(Product item : items) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void removeById(int id) {
+        Product[] tmp = new Product[items.length - 1];
+        int copyToIndex = 0;
+        if (findById(id) != null) {
+            for (Product item : items) {
+                if (item.getId() != id) {
+                    tmp[copyToIndex] = item;
+                    copyToIndex++;
+                }
+            }
+            items = tmp;
+        }
+    }
+}
